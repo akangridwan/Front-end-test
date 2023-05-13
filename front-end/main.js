ef32d3d24cffb1edd62c75efdb7309bab50da502
Ridwan <116270461+akangridwan@users.noreply.github.com>
2023-05-13 14:48:35 +0700

    Delete front-end directory

--- front-end/main.js
+++
@@ -1,7 +0,0 @@
-// nav
-let btnNav= document.querySelector('.header nav .btn-nav'),
-    navMenu= document.querySelector('.header nav .nav-menu');
-
-    btnNav.addEventListener('click', ()=>{
-        navMenu.classList.toggle('show-drop');
-    })
